c1f5533dc653894a9a4467532cb072bb6aa7bb9b
KirillGul <guliev.kirill@yandex.ru>
2019-06-10 12:41:17 +0300

    Не доделанный урок 7

--- lean7.php
+++ lean7.php
@@ -41,3 +41,72 @@ if (strlen($password) > 5 and strlen($password) < 10)
 else 
     echo 'Нужно придумать другой пароль';
 echo '<br><br>';
+
+echo 'Задача №9 -<br>Решение:<br>';
+$str = 'html css php';
+echo substr($str, 0, 4).'<br>';
+echo substr($str, 5, 3).'<br>';
+echo substr($str, 9).'<br>';
+echo '<br><br>';
+
+echo 'Задача №10 -<br>Решение:<br>';
+$str = 'Hello, world.';
+echo substr($str, -3).'<br>';
+echo '<br><br>';
+
+echo 'Задача №11 -<br>Решение:<br>';
+$str = 'http://10.91.191.5/index.php/telefonnyj-spravochnik';
+if (substr($str, 0, 7) == 'http://') echo 'Да'; else echo 'Нет';
+echo '<br><br>';
+
+echo 'Задача №12 -<br>Решение:<br>';
+$str = 'http://10.91.191.5/index.php/telefonnyj-spravochnik';
+if (substr($str, 0, 7) == 'http://' or substr($str, 0, 8) == 'https://')
+    echo 'Да';
+else
+    echo 'Нет';
+echo '<br><br>';
+
+echo 'Задача №13 -<br>Решение:<br>';
+$str = 'kartinka.png';
+if (substr($str, -4) == '.png') echo 'Да'; else echo 'Нет';
+echo '<br><br>';
+
+echo 'Задача №14 -<br>Решение:<br>';
+if (substr($str, -4) == '.png' or substr($str, -4) == '.jpg')
+    echo 'Да';
+else 
+    echo 'Нет';
+echo '<br><br>';
+
+echo 'Задача №15 -<br>Решение:<br>';
+$str = 'fsdfsd';
+if (strlen($str) > 5) {
+    echo substr($str, 0, 5).'...';
+} elseif (strlen($str) <= 5) {
+    echo $str;
+}
+echo '<br><br>';
+
+echo 'Задача №16 -<br>Решение:<br>';
+$str = '31.12.2013';
+echo str_replace('.', '-', $str);
+echo '<br><br>';
+
+echo 'Задача №17 -<br>Решение:<br>';
+$str = 'aabbc';
+echo str_replace(['a', 'b', 'c'], [1, 2, 3], $str);
+echo '<br><br>';
+
+echo 'Задача №18 -<br>Решение:<br>';
+$str = '1a2b3c4b5d6e7f8g9h0';
+echo str_replace([1,2,3,4,5,6,7,8,9,0], '', $str);
+echo '<br><br>';
+
+echo 'Задача №19 -<br>Решение:<br>';
+$str = 'aabbc';
+echo strtr ($str, ['a'=>'1', 'b'=>'2', 'c'=>'3']);
+echo '<br><br>';
+
+echo 'Задача №20 -<br>Решение:<br>';
+echo '<br><br>';
