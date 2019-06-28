0a9b4f1c5ce7ea4b723338d05a05333f5156c6cd
Kirill <guliev.kirill@yandex.ru>
2019-06-28 18:09:44 +0300

    Готовый урок 12

---
+++ lean12.php
@@ -0,0 +1,30 @@
+<?php
+echo 'Задача №1 -<br>Решение:<br>';
+function squareOfNumber($a) {
+    return $a*$a;
+}
+echo squareOfNumber(2);
+echo '<br><br>';
+
+echo 'Задача №2 -<br>Решение:<br>';
+function sumOfNumber($a, $b) {
+    return $a+$b;
+}
+echo sumOfNumber(2, 2);
+echo '<br><br>';
+
+echo 'Задача №3 -<br>Решение:<br>';
+function func1($a, $b, $c) {
+    return ($a - $b) / $c;
+}
+echo func1(6, 3, 1);
+echo '<br><br>';
+
+echo 'Задача №4 -<br>Решение:<br>';
+function func2($a) {
+    $week = [1=>'пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+    return $week[$a];
+}
+echo func2(6);
+echo '<br><br>';
+
